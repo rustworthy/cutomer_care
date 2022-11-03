69f01927abaccc076754835b30d9fdc7c2387c34
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-11-03 10:59:14 +0300

    Add pagination query params support.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fmt::Display;
+use std::hash::Hash;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 use warp::cors::CorsForbidden;
+use warp::reject::Reject;
 use warp::{http, Filter};
 use warp::{Rejection, Reply};
 
@@ -28,21 +31,87 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-async fn list_guestions(store: Store) -> Result<impl warp::Reply, warp::Rejection> {
-    let res: Vec<Question> = store.questions.values().cloned().collect();
-    Ok(warp::reply::json(&res))
+#[derive(Debug)]
+enum ServiceError {
+    ParseError(std::num::ParseIntError),
+    MissingParams,
+    InvalidParamsRange,
+}
+
+impl Reject for ServiceError {}
+impl Display for ServiceError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ServiceError::ParseError(err) => write!(f, "Failed to parse parameter: {}", err),
+            ServiceError::MissingParams => write!(f, "Missing parameter"),
+            ServiceError::InvalidParamsRange => write!(f, "Invalid parameters range"),
+        }
+    }
+}
+
+struct Pagination {
+    start: usize,
+    end: usize,
+}
+
+impl Pagination {
+    fn parse_from_map(params: HashMap<String, String>) -> Result<Self, ServiceError> {
+        if !params.contains_key("start") || !params.contains_key("end") {
+            return Err(ServiceError::MissingParams);
+        }
+
+        let start = params
+            .get("start")
+            .unwrap()
+            .parse::<usize>()
+            .map_err(ServiceError::ParseError)?;
+        let end = params
+            .get("end")
+            .unwrap()
+            .parse::<usize>()
+            .map_err(ServiceError::ParseError)?;
+
+        if start > end {
+            return Err(ServiceError::InvalidParamsRange);
+        }
+
+        Ok(Self { start, end })
+    }
+}
+
+async fn list_guestions(
+    params: HashMap<String, String>,
+    store: Store,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let unpaginated_quests: Vec<Question> = store.questions.values().cloned().collect();
+    if params.is_empty() {
+        return Ok(warp::reply::json(&unpaginated_quests));
+    }
+    let pgn = Pagination::parse_from_map(params)?;
+    if pgn.end >= unpaginated_quests.len() {
+        return Ok(warp::reply::json(&unpaginated_quests));
+    }
+    let requested_chunk = &unpaginated_quests[pgn.start..pgn.end];
+    Ok(warp::reply::json(&requested_chunk))
 }
 
 async fn handle_err(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(error) = r.find::<CorsForbidden>() {
+    if let Some(err) = r.find::<CorsForbidden>() {
         return Ok(warp::reply::with_status(
-            error.to_string(),
+            err.to_string(),
             http::StatusCode::FORBIDDEN,
         ));
     }
 
+    if let Some(err) = r.find::<ServiceError>() {
+        return Ok(warp::reply::with_status(
+            err.to_string(),
+            http::StatusCode::RANGE_NOT_SATISFIABLE,
+        ));
+    }
+
     Ok(warp::reply::with_status(
-        "Route not found".to_string(),
+        "Not found".to_string(),
         http::StatusCode::NOT_FOUND,
     ))
 }
@@ -77,6 +146,7 @@ async fn main() {
     let list_quest = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
+        .and(warp::query())
         .and(store_filter)
         .and_then(list_guestions)
         .recover(handle_err);
