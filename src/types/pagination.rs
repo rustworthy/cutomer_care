73d5a9b26b08c624f21db4bd366c727e443fd9e2
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-11-04 13:48:20 +0300

    Add sample code to pagination parse_from_map

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -7,6 +7,15 @@ pub struct Pagination {
 }
 
 impl Pagination {
+    /// ## Example usage
+    /// ```rust
+    /// let query_string_params = HashMap::new();
+    /// query_string_params.push("start", "1");
+    /// query_string_params.push("end", "100");
+    /// let pagination = types::pagination::Pagination::parse_from_map(query_string_params).unwrap();
+    /// assert_eq!(pagination.start, 1);
+    /// assert_eq!(pagination.start, 100);
+    /// ```
     pub fn parse_from_map(params: HashMap<String, String>) -> Result<Self, ServiceError> {
         if !params.contains_key("start") || !params.contains_key("end") {
             return Err(ServiceError::MissingParams);
